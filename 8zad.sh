b9aa5fe41724464a0a893c4702f3ada559c5a234
Jakub Bełcik <belcik.jakub@gmail.com>
2010-12-12 17:36:04 +0100

    rename - test

--- 8zad.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-last -400 | grep pts | sed -E 's/  +/ /g' | cut -d \  -f 1 | sort | uniq -c | sort -r | head -3
--- lastroom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-Host=`last $1 | head -n 1 | sed -E 's/  +/ /g' | cut -d \  -f 3`
-
-echo "   $Host"
