4a4b5e28e52a3d05b61777084a8072af34e7800c
Jakub Bełcik <belcik.jakub@gmail.com>
2010-12-12 17:41:32 +0100

    zmiana

---
+++ 4lab**zad.sh
@@ -0,0 +1,4 @@
+Elementy zbioru ustawiamy rosn±co lub malej±co.
+Moc zbioru dzielimy na pó zaokr±glamy w gódo czêi ca³kowitej.
+Je¿eli warto¶æyliczonego elementu bêie za du¿a to ponawiamy operacjê pierwszej po³ó, je¿eli za ma³a to w drugiej.
+Powtarzamy t± czynno¶æ¿ trafimy na w³a¶ciwy wynik.
